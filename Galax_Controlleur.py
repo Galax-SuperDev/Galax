a9f054ffd4535e0e42b224aca98ee44f58a710a2
xavierhd <xavierhd@hotmail.com>
2014-04-20 20:50:59 -0400

    Contrôleur
    Contrôleur contrôlant

--- Galax_Controlleur.py
+++ Galax_Controlleur.py
@@ -3,18 +3,15 @@ import Galax_Vue
 
 class Controlleur:
     def __init__(self):
-        self.debugMod = True#ce mod sera a enlever lorsqu'il sera possible de ne pas passer en parametre dès le init du controleur un self.jeu.getMergedListeEtoile à la vue
-        if(self.debugMod):
-            self.jeu = Galax_Modele.Jeu()
-        else:
-            self.jeu = None
-        self.vue = Galax_Vue.Vue(self.jeu.getMergedListeEtoile())
+        self.jeu = None
+        self.vue = Galax_Vue.Vue(self)
         self.vue.root.mainloop()
 
     def menuLoop(self, vueReturnKey):
         if(vueReturnKey == 0):#NewGame
             self.jeu = Galax_Modele.Jeu()
-            #call de la fonction dans la vue qui va afficher le jeux
+            vue.setListeEtoile(self.jeu.getMergedListeEtoile())
+            vue.drawJeu()
         elif(vueReturnKey == 1):#High scores
             pass
         elif(vueReturnKey == 2):#Quitter
@@ -22,10 +19,20 @@ class Controlleur:
 
 		
     def gameLoop(self):
+        jeu.gestionTroupe()
         for i in range(10):
             self.jeu.moveFlotteEnMouvement()
         self.jeu.anneePassees +=1
-
+        if(jeu.faction[0].isDead()):
+            vue.drawFinPartie(False)
+        if(jeu.faction[1].isDead() and jeu.faction[1].isDead()):
+            vue.drawFinPartie(True)
+            vue.drawMainMenu()
+        if(jeu.faction[1].isDead()):
+            vue.setFactionVaincue(True)
+        if(jeu.faction[1].isDead()):
+            vue.setFactionVaincue(False)
+            
 
 if __name__ == '__main__':
     c=Controlleur()
