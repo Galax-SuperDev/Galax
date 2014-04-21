ca32f27eafb6a3796f9784f48dec2b9ed1ca9905
tsuzumiko <montmorencyfelix@gmail.com>
2014-04-20 20:48:37 -0400

    la vue
    basic update 01

--- Galax_Vue.py
+++ Galax_Vue.py
@@ -1,10 +1,12 @@
 from tkinter import *
+import time
 
 class Vue:
-    def __init__(self,listeEtoile):
-        self.listeEtoiles = listeEtoile
+    def __init__(self,):
+        self.factionVaincue=''
+        self.listeEtoile=[]
+        
         self.root=Tk()
-              
         self.root.title('Galax')
         self.root.iconbitmap(default='galaxIcon.ico')
               
@@ -14,32 +16,21 @@ class Vue:
         self.height = 640
         self.canvas=Canvas(self.root, width=self.screenWidth, height=self.screenHeight, bg='black')  
               
-        self.canvas.bind('<Configure>', self.resize)
+        #self.canvas.bind('<Configure>', self.resize)
         self.canvas.bind('<Button-1>', self.click)
     
-        self.canvas.pack(expand=True, fill=BOTH)
-        
-        self.drawMenu()
-        '''
-        self.canvas.create_rectangle(0,self.screenHeight-128,
-                             self.screenWidth,self.screenHeight,
-                             fill='gray',tags='menuBar')
-        
-        self.canvas.create_rectangle(self.screenWidth-256,0,
-                                     self.screenWidth,self.screenHeight-256,
-                                     fill='gray',tags='menuBar')
-        
+        #self.canvas.pack(expand=True, fill=BOTH)
+    
+        #image de background
         self.background = PhotoImage(file="cosmosBG.gif")
-        #self.background.zoom(self.width, self.height)
         self.canvas.create_image(0,0,anchor=NW,image=self.background)
-
-        self.drawEtoiles()
-        '''
+        
+        
     def click(self,event):
         eventX = event.x
         eventY = event.y
-        print(str(eventX)+str(eventY))
-    
+        
+    '''
     def resize(self,event):
         self.screenWidth = event.width
         self.screenHeight = event.height
@@ -47,19 +38,52 @@ class Vue:
         self.height = self.screenHeight-128;
         
         self.drawMenu()
+    '''
+#----------------------------------------------------------------------------------------
+#---------------------   JEU  -----------------------------------------------------------  
+#----------------------------------------------------------------------------------------
+
+    #bool 0 = czin 1= gubru
+    def setFactionVaincue(self,factionVaincue):
+        self.factionVaincue = factionVaincue
         
-        '''
+    #la liste de toutes les etoiles de la partie
+    def setListeEtoile(self,listeEtoile):
+        self.listeEtoile = listeEtoile
+        
+    def drawJeu(self):
+        self.drawEtoiles(self.listeEtoile)
+        self.drawBottomMenu()
+        self.drawSideMenu()
+        
+    def drawSideMenu(self):
+        self.canvas.delete("menuBar")
+        self.canvas.create_rectangle(self.screenWidth-256,0,
+                                     self.screenWidth,self.screenHeight-128,
+                                     fill='gray',tags="menuBar")
+        
+    def drawBottomMenu(self):
         self.canvas.delete("menuBar")
         self.canvas.create_rectangle(0,self.screenHeight-128,
                                      self.screenWidth,self.screenHeight,
                                      fill='gray',tags="menuBar")
-        
-        self.canvas.create_rectangle(self.screenWidth-256,0,
-                                     self.screenWidth,self.screenHeight-128,
-                                     fill='gray',tags="menuBar")
-        '''
-        
-    def drawMenu(self):
+
+    def drawEtoiles(self):
+        for e in self.listeEtoiles:
+            posX = int((e.posX*self.width-100)/100)
+            posY = int((e.posY*self.height-100)/100)
+            self.canvas.create_oval(posX,posY,posX+32,posY+32,fill='green',tags='etoiles')
+            
+#----------------------------------------------------------------------------------------
+#---------------------   JEU  -----------------------------------------------------------  
+#----------------------------------------------------------------------------------------
+
+
+            
+#----------------------------------------------------------------------------------------
+#---------------------   MAIN MENU ------------------------------------------------------  
+#----------------------------------------------------------------------------------------
+    def drawMainMenu(self):
         
         self.buttonWidth = 200
         self.buttonHeight = 64
@@ -95,10 +119,24 @@ class Vue:
                                 fill='black',activefill='white',
                                 font=('consolas','16'),
                                 tags='menu')
+
+#----------------------------------------------------------------------------------------
+#---------------------   MAIN MENU ------------------------------------------------------  
+#----------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------
+#---------------------   END GAME ------------------------------------------------------- 
+#----------------------------------------------------------------------------------------
+    # bool 1 = win 0 = lose
+    def drawFinPartie(self,etatFinale):
+        self.canvas.delete('all')
         
-    def drawEtoiles(self):
-        for e in self.listeEtoiles:
-            posX = int((e.posX*self.width-100)/100)
-            posY = int((e.posY*self.height-100)/100)
-            self.canvas.create_oval(posX,posY,posX+32,posY+32,fill='green')
-            print(str(posX)+' '+str(posY))
+        if(etatFinale == 0):
+            self.canvas.create_text(100,100,text='vous avez perdu la partie')
+        elif(etatFinale == 1):
+            self.canvas.create_text(100,100,text='vous avez gagne la partie')
+        time.sleep(5)
+        
+#----------------------------------------------------------------------------------------
+#---------------------   END GAME ------------------------------------------------------- 
+#----------------------------------------------------------------------------------------
